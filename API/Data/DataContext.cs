eddb3b900ca4ff1777cae25bea781d3463357324
Rijad Maljanović <rijad.maljanovic@edu.fit.ba>
2020-12-05 23:29:16 +0100

    Commitment

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<AppUser>().HasData(
+                new AppUser
+                {
+                    ID = 1,
+                    UserName = "Tom"
+                });
+
+        }
+    }
+}
---
+++ API/Migrations/20201201112256_AddSeed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AddSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "ID", "UserName" },
+                values: new object[] { 1, "Tom" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 1);
+        }
+    }
+}
